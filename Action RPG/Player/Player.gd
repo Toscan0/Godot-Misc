b63950ae3e22c7fd8b1d3286b880571f3a314bd7
Toscan0 <j.tiago.henriques@tecnico.ulisboa.pt>
2021-05-14 17:49:56 +0100

    proj updated

--- Action RPG/Player/Player.gd
+++ Action RPG/Player/Player.gd
@@ -1,16 +1,22 @@
 extends KinematicBody2D
 
-export var sensitivity = 4
+export var sensitivity = 1
+export var acceleration = 10
+export var friction = 10
+export var max_speed = 100
+
 var vel = Vector2.ZERO
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
-	input_vector.x = (Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")) * sensitivity
-	input_vector.y = (Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")) * sensitivity
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
-		vel = input_vector
+		vel += input_vector * acceleration * delta
+		vel = vel.clamped(max_speed * delta)
 	else:
-		vel = Vector2.ZERO
+		vel = vel.move_toward(Vector2.ZERO, friction * delta)
 		
-	move_and_collide(vel)
+	vel = move_and_slide(vel * sensitivity)
