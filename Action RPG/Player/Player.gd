7c2a23e5cd25e0a8f6b6de2c60092a4c58405591
Toscan0 <j.tiago.henriques@tecnico.ulisboa.pt>
2021-05-17 11:22:01 +0100

    anim done

--- Action RPG/Player/Player.gd
+++ Action RPG/Player/Player.gd
@@ -1,11 +1,15 @@
 extends KinematicBody2D
 
-export var sensitivity = 1
+
 export var acceleration = 10
 export var friction = 10
 export var max_speed = 100
 
 var vel = Vector2.ZERO
+
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
@@ -14,8 +18,12 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/Iddle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationState.travel("Run")
 		vel = vel.move_toward(input_vector * max_speed, acceleration * delta)
 	else:
+		animationState.travel("Iddle")
 		vel = vel.move_toward(Vector2.ZERO, friction * delta)
 		
 	vel = move_and_slide(vel)
