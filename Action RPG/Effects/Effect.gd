f6d25b478c3fb68612c3d829a2900b52c3ce0749
Toscan0 <j.tiago.henriques@tecnico.ulisboa.pt>
2021-05-24 16:29:52 +0100

    stuff

--- Action RPG/Effects/Effect.gd
+++ Action RPG/Effects/Effect.gd
@@ -2,7 +2,6 @@ extends AnimatedSprite
 
 func _ready():
 	connect("animation_finished", self, "_on_animation_finished")
-	frame = 0
 	play("Destroy")
 
 
--- Action RPG/Enemies/Bat.gd
+++ Action RPG/Enemies/Bat.gd
@@ -1,11 +1,25 @@
 extends KinematicBody2D
 
+const EnemyDeathEfx = preload("res://Effects/EnemyDeathEffect.tscn")
+
+enum{
+	IDLE,
+	WANDER,
+	CHASE
+}
+
 export var knockback_strengh = 100
 export var friction = 200
+export var acceleration = 300
+export var max_speed = 50
 
 var knockback = Vector2.ZERO
+var vel = Vector2.ZERO
+var state = IDLE
 
 onready var stats = $Stats
+onready var playerDetection = $PlayerDetectionZone
+onready var sprite = $AnimSprite
 
 func _ready():
 	print(stats.max_health)
@@ -18,6 +32,30 @@ func _on_Hurtbox_area_entered(area):
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, friction * delta)
 	knockback = move_and_slide(knockback)
-
+	
+	match state:
+		IDLE:
+			vel = vel.move_toward(Vector2.ZERO, friction * delta)
+			seek_player()
+		WANDER:
+			pass
+		CHASE:
+			var player = playerDetection.player
+			if player != null:
+				var dir = (player.global_position - global_position).normalized()
+				vel = vel.move_toward(dir * max_speed, acceleration * delta)
+			else:
+				state = IDLE
+	
+	sprite.flip_h = vel.x < 0
+	vel = move_and_slide(vel)
+		
+func seek_player():
+	if playerDetection.can_see_player():
+		state = CHASE
+	
 func _on_Stats_no_health():
 	queue_free()
+	var enemyDeathEfx = EnemyDeathEfx.instance()
+	get_parent().add_child(enemyDeathEfx)
+	enemyDeathEfx.global_position = global_position
