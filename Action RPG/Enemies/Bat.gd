0d3eeb1762384ef0b2ad3dde5c3cdec960dbbe7d
Toscan0 <j.tiago.henriques@tecnico.ulisboa.pt>
2021-05-24 15:22:17 +0100

    stuff

--- Action RPG/Enemies/Bat.gd
+++ Action RPG/Enemies/Bat.gd
@@ -5,10 +5,19 @@ export var friction = 200
 
 var knockback = Vector2.ZERO
 
-func _on_Hurtbox_area_entered(area):
-	knockback = area.knockback_vector * knockback_strengh
+onready var stats = $Stats
 
+func _ready():
+	print(stats.max_health)
+	print(stats.health)
+	
+func _on_Hurtbox_area_entered(area):
+	stats.health -= area.damage
+	knockback = area.knockback_vector * knockback_strengh
 
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, friction * delta)
 	knockback = move_and_slide(knockback)
+
+func _on_Stats_no_health():
+	queue_free()
--- Action RPG/Player/SwordHitbox.gd
+++ Action RPG/Player/SwordHitbox.gd
@@ -1,3 +1,3 @@
-extends Area2D
+extends "res://World/Hitbox.gd"
 
 var knockback_vector = Vector2.ZERO
