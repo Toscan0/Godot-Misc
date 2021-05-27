6975bd76277859fcb1dd00ae483c337807c434e2
Toscan0 <j.tiago.henriques@tecnico.ulisboa.pt>
2021-05-27 17:55:14 +0100

    path follow

--- PathFollow/Scenes/PathFollow2D.gd
+++ PathFollow/Scenes/PathFollow2D.gd
@@ -6,14 +6,10 @@ var pathLength = 10
 var canMove = false
 
 func _process(delta):
-	if canMove:
-		var i = float(5)/float(pathLength)
-		set_unit_offset(i)
-		print(i)
-	#set_offset(get_offset() + runSpeed * delta)
+	set_offset(get_offset() + runSpeed * delta)
 	
-	#if(loop == false and get_unit_offset() == 1):
-		#queue_free()
+	if(loop == false and get_unit_offset() == 1):
+		queue_free()
 
 func _on_Dado_dice_used(value):
 		print(value)
